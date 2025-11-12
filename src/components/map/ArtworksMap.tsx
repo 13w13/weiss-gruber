fc8dce96c250bcf3e1249774dd3c09d5d14da63c
13w <13w13>
2025-11-12 20:39:16 +0100

    Fix JSX: remove stray </li></ul> in decade filter panel

--- src/components/map/ArtworksMap.tsx
+++ src/components/map/ArtworksMap.tsx
@@ -248,9 +248,6 @@ export default function ArtworksMap({ works }: { works: Vitrail[] }) {
                 );
               })}
             </div>
-                </li>
-              ))}
-            </ul>
           </div>
         )}
       </div>
