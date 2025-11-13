388ea37fc9e2141ed431ef951d9f432bfe2710d6
13w <13w13>
2025-11-13 12:33:25 +0100

    add caption

--- src/pages/jeannette/catalogue/[id].tsx
+++ src/pages/jeannette/catalogue/[id].tsx
@@ -11,6 +11,7 @@ import Lightbox from 'yet-another-react-lightbox';
 import Zoom from 'yet-another-react-lightbox/plugins/zoom';
 import Captions from 'yet-another-react-lightbox/plugins/captions';
 import 'yet-another-react-lightbox/styles.css';
+import 'yet-another-react-lightbox/plugins/captions.css';
 import { Vitrail } from '@/types/images';
 
 interface CsvRow {
