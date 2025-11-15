ec3952b08f8b12497cf28999cfcad2f4a2c0a0b5
13w <13w13>
2025-11-15 19:04:13 +0100

    update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="fr">
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        
+        {/* Meta tags */}
+        <meta name="description" content="Site dédié à Jeannette Weiss-Gruber, peintre-verrier française. Catalogue raisonné de ses créations de vitraux." />
+        <meta property="og:title" content="Jeannette Weiss-Gruber - Peintre-verrier" />
+        <meta property="og:description" content="Catalogue raisonné des œuvres de Jeannette Weiss-Gruber, peintre-verrier française" />
+        <meta property="og:image" content="/og-image.jpg" />
+        <meta property="og:type" content="website" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
