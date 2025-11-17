94469c96581ded78968f702184677577adaee5a6
13w <13w13>
2025-11-17 14:08:05 +0100

    clean step 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
-import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 
 export default function HomePage() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/jeannette')
+  }, [router])
+
   return (
     <>
       <Head>
